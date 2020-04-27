198a7ed80e6961f9b9e4a92e52200da4b5fd9207
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-04-27 16:39:07 +0100

    Started communication between client and server processes

--- src/src-u1/main.c
+++ src/src-u1/main.c
@@ -1,5 +1,18 @@
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
 
 int main(int argc, char* argv[]) {
+    int publicFD;
+    char fifoName[] = "fifo"; // TODO: get fifoname from command line argument parsing
+
+    // Open FIFO for requests to the server
+    do {
+        publicFD = open(fifoName, O_WRONLY);
+        if (publicFD < 0) sleep(1);
+    } while (publicFD < 0);
+
     return 0;
 }

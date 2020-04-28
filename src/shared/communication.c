d35896fbfa1a164e541d65e76ee72797bcfe66c5
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-04-28 22:24:48 +0100

    Small changes to Message struct

--- src/shared/communication.c
+++ src/shared/communication.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 void printMessage(const Message* message) {
-    printf("i: %lu\npid: %d\ntid: %d\ndur: %d\npl: %d\n", message->i, message->pid, 
+    printf("i: %lu\npid: %d\ntid: %lu\ndur: %d\npl: %d\n", message->i, message->pid, 
             message->tid, message->dur, message->pl);
 }
--- src/shared/communication.h
+++ src/shared/communication.h
@@ -3,16 +3,18 @@
 
 #include <stdlib.h>
 
+#define MILLI_TO_MICRO 1000
+
 /**
  * @brief Structure that holds information for a message between the client and the server.
  * Can be used for requests or responses.
  */
 typedef struct Message {
-    size_t i;   /** Request number */
-    pid_t pid;  /** Process ID of the client (for requests) or the server (for response) */
-    pid_t tid;  /** Thread ID */
-    int dur;    /** Duration of the access in milliseconds, -1 if access is denied */
-    int pl;     /** Place number, -1 for requests or for responses if access is denied */
+    size_t i;       /** Request number */
+    pid_t pid;      /** Process ID of the client (for requests) or the server (for response) */
+    pthread_t tid;  /** Thread ID */
+    int dur;        /** Duration of the access in milliseconds, -1 if access is denied */
+    int pl;         /** Place number, -1 for requests or for responses if access is denied */
 } Message;
 
 void printMessage(const Message* message);
